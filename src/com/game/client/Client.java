649b87b5d5d5efffe8ea1eebe612648c0e5b0fdd
Daniel Lasche <dlasche@gmail.com>
2021-04-25 19:11:21 -0700

    Changes comment

--- src/com/game/client/Client.java
+++ src/com/game/client/Client.java
@@ -2,6 +2,6 @@ package com.game.client;
 
 public class Client {
     public static void main(String[] args) {
-        System.out.println("you got this");
+        System.out.println("you got this. Edit #2");
     }
 }
